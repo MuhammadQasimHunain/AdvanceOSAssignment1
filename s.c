9f7971470d2af52703417cb8caa26af95a77f1e0
Hassaan Fayyaz <hassaanfayyaz@gmail.com>
2017-02-26 03:20:00 +0500

    - Thread creation improved. Code cleanup!

--- s.c
+++ s.c
@@ -38,7 +38,7 @@ struct Thread
 	int socketId;
 };
 
-struct ThreadPool
+struct ThreadPoolManager
 {
 	struct Thread threadArr[1024];
 	int totalThreadCount; 
@@ -46,7 +46,7 @@ struct ThreadPool
 
 void *handle_request(void *param);
 void send_file(char *fileName, int new_sd);
-int createThreadPool(struct Thread *pool);
+int createThreadPool(struct ThreadPoolManager *manager);
 
 int myReceive(int socket, char *arr, int length, int flag);
 int mySend(int socket, char *arr, int length, int flag);
@@ -63,7 +63,8 @@ int main()
 	int threadCount = 0;
 	char message1[1024] = "Thread1";
 	char message2[1024] = "Thread2";
-	struct Thread threadPool[1024];
+	struct ThreadPoolManager tm;
+
 
 	int s_id;
 	char msg[1024];
@@ -74,10 +75,8 @@ int main()
 	my_addr.sin_port = htons(5555);	//assigning the port
 	my_addr.sin_addr.s_addr = inet_addr("127.0.0.1");	//assigning the self ip address
 
-	if(bind(s_id, (struct sockaddr*)&my_addr, sizeof(my_addr) ) == -1)
-	{
+	if(bind(s_id, (struct sockaddr*)&my_addr, sizeof(my_addr) ) == -1) {
 		error("Server failed to bind\n");
-		perror("Error");
 	}
 	listen(s_id,2); 
 	unsigned int size = sizeof (struct sockaddr_in);
@@ -87,44 +86,26 @@ int main()
 	int send_id=0;
 	char data[1024];
 
-	createThreadPool(threadPool);
-
-		// pthread_create(&threads[0], NULL, handle_request, (void*) NULL);
-		// {
-		// 	printf("Cannot create %d th thread\n", 1);
-		// }
-		// else 
-		// {
-		// 	printf("Created %d th thread\n", 1);
-		// }
+	createThreadPool(&tm);
 
 	while(1)
 	{
 		// accepting the client's request and assigning new socket
 		new_sd = accept (s_id, (struct sockaddr *) &remote_addr, &size);
-		if(new_sd == -1)
-		{
+		if(new_sd == -1) {
 			error("Server. Could not accept the request");
 			return 0;
 		}
 			// for(int i=0 ; i < 10 ; i++) 
 	// {
-		threadPool[threadCount].socketId = new_sd;
+		tm.threadArr[threadCount].socketId = new_sd;
 		if (pthread_cond_signal(&cond[threadCount]) != 0) {
 		perror("pthread_cond_signal() error");
 		// exit(4);
 		}
-		else {
-			printf("Sending Signal\n");
-		}
 		threadCount++;
 	// }
 
-
-		// struct Details *myDetails = malloc (sizeof(struct Details));
-		// myDetails->send_id = new_sd;
-		// myDetails->thread_number = threadCount;
-		// rv1 = pthread_create(&threads[threadCount++], NULL, handle_request, (void*) myDetails);
 	}
 
 	return 0;
@@ -142,18 +123,14 @@ void *handle_request(void *param)
 	}
 
 	if (pthread_cond_wait(&cond[threadDetails->threadNumber], &mutex[threadDetails->threadNumber]) != 0) {
-    perror("pthread_cond_timedwait() error");
-    // exit(7);
-	}
-	else {
-		printf("Conditional wait\n");
+	    perror("pthread_cond_timedwait() error");
 	}
 
 	int new_sd=threadDetails->socketId;
 	int recv_id=0;
 	int send_id=0;
 	char fileAddress[1024];
-	char welcome[1024] = "\n***** Worker thread number : ";
+	char welcome[1024] = "***** Worker thread number : ";
 	printf("%s %d  *****\n", welcome, threadDetails->threadNumber);
 	
 
@@ -164,55 +141,46 @@ void *handle_request(void *param)
 
 	// send_id = mySend(new_sd, QUIT ,100, 0);
 	// close(new_sd);
-	printf("\nTerminating worker thread\n");
+	printf("Terminating worker thread\n\n");
 	return NULL;
 }
 
 
 
-
-
 //****************** THREAD MANAGEMENT ******************//
 
-int createThreadPool(struct Thread *pool)
+int createThreadPool(struct ThreadPoolManager *manager)
 {
 
 
 	for(int i=0 ; i < 102 ; i++) 
 	{
 		if (pthread_mutex_init(&mutex[i], NULL) != 0) {
-	    perror("pthread_mutex_init() error");
-	    // exit(1);
-	  }
+		    perror("pthread_mutex_init() error");
+		}
 		if (pthread_cond_init(&cond[i], NULL) != 0) {
-		perror("pthread_cond_init() error");
-		// exit(2);
+			perror("pthread_cond_init() error");
 		}
 		if (pthread_mutex_lock(&mutex[i]) != 0) {
-		perror("pthread_mutex_lock() error");
-		// exit(6);
+			perror("pthread_mutex_lock() error");
 		}
 	}
   	pthread_attr_t attr;
 	pthread_attr_init(&attr); // Required!!!
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
 	pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);
-	// pthread_create(&tid, &attr, foo, NULL);
 
-	struct Thread *threadPool = pool;
+	struct Thread *threadPool = manager->threadArr;
 	threadPool[0].threadId = 0;
 
-	for(int i=0 ; i < 10 ; i++) 
-	{
+	for(int i=0 ; i < 10 ; i++) {
 		struct Details *myDetails = malloc (sizeof(struct Details));
 		myDetails->thread_number = i;
 		threadPool[i].threadNumber = i;
-		if (pthread_create(&threadPool[i].threadId, &attr, handle_request, (void*) &threadPool[i]) != 0)
-		{
+		if (pthread_create(&threadPool[i].threadId, &attr, handle_request, (void*) &threadPool[i]) != 0) {
 			printf("Cannot create %d th thread\n", i+1);
 		}
-		else 
-		{
+		else {
 			printf("Created %d th thread\n", i+1);
 		}
 	}
@@ -227,51 +195,51 @@ int createThreadPool(struct Thread *pool)
 
 void send_file(char *fileName, int new_sd)
 {
-    char data[1024] ;
-    // char fileName[1024] ;
+//     char data[1024] ;
+//     // char fileName[1024] ;
     
-    char temp[1024];
+//     char temp[1024];
     
     
-    int send_id;
-    FILE *fileptr = NULL;
-    fileptr = fopen(fileName, "rb"); // read binary mode
-    if(!fileptr)
-    {
-        error("test.pdf could not open in binary mode");
-        return ;
-    }
+//     int send_id;
+//     FILE *fileptr = NULL;
+//     fileptr = fopen(fileName, "rb"); // read binary mode
+//     if(!fileptr)
+//     {
+//         error("test.pdf could not open in binary mode");
+//         return ;
+//     }
     
-    // keep reading from the file until the file is complete
-    while(fread(data, 1, 1000, fileptr) != NULL)
-    {
-        data[1023]='\0';
-        send_id = write(new_sd, data ,1000);
-        if(send_id == -1)
-        {
-            printf("Server. Could not send");
-            perror("Server. Could not send");
-            //			return 0;
-        }
+//     // keep reading from the file until the file is complete
+//     while(fread(data, 1, 1000, fileptr) != NULL)
+//     {
+//         data[1023]='\0';
+//         send_id = write(new_sd, data ,1000);
+//         if(send_id == -1)
+//         {
+//             printf("Server. Could not send");
+//             perror("Server. Could not send");
+//             //			return 0;
+//         }
         
-    }
-    int i=0;
-    for(i=0 ; i<1024 ; i++)
-        data[i] = '\0';
-    send_id = write(new_sd, data ,1024);
-    if(send_id == -1)
-    {
-        error("Server. Could not send");
-        return ;
-    }
-    else
-        printf("%s File Successfully Sent\n", fileName);
+//     }
+//     int i=0;
+//     for(i=0 ; i<1024 ; i++)
+//         data[i] = '\0';
+//     send_id = write(new_sd, data ,1024);
+//     if(send_id == -1)
+//     {
+//         error("Server. Could not send");
+//         return ;
+//     }
+//     else
+//         printf("%s File Successfully Sent\n", fileName);
     
     
-    fclose(fileptr);	// closing the file
+//     fclose(fileptr);	// closing the file
     
     
-    // close(s_id);	//closing the socket
+//     // close(s_id);	//closing the socket
 }
 
 
