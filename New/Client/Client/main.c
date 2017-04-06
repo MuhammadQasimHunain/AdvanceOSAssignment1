41ca92101969ae614493222171ac7baa452086ee
Muhammad Qasim Hunain <muhammad.qasim1255@hotmail.com>
2017-04-06 19:59:28 +0500

    Separate Mine Projects

---
+++ New/Client/Client/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Client
+//
+//  Created by Apple PC on 06/04/2017.
+//  Copyright © 2017 ArhamSoft. All rights reserved.
+//
+
+#include <stdio.h>
+
+//int main(int argc, const char * argv[]) {
+//    // insert code here...
+//    printf("Hello, World!\n");
+//    return 0;
+//}
